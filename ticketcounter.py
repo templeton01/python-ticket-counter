22f6cfa9fb0631c1e2dc5dc4f5f9edc4373d57b2
Nkoatsi Templeton Nqalanga <templetonmqalanga@gmail.com>
2026-07-29 16:38:32 +0200

    the ticket counter
    1. It let the user book the ticket.
    2. Shows the user artist wanted to see

---
+++ ticketcounter.py
@@ -0,0 +1,6 @@
+#The ticket counter
+# Data input for the ticket counter pragram
+name = input("Please enter your name:") #Input for the user's name
+name_band_or_artist = input("Please enter the name of the band or artist you want to see:") #input for the band or artist that the user wants to see
+# Out put for the ticket counter program
+print(f"Hey,{name} Your ticket to see {name_band_or_artist} is booked successfully!")
